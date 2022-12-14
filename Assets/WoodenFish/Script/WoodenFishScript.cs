2a32037c20e6954cbb42e8f8de2ac6920efe5e6c
林品辰 <s10325347@gmail.com>
2022-12-15 01:47:42 +0800

    First Push

---
+++ Assets/WoodenFish/Script/WoodenFishScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WoodenFishScript : MonoBehaviour {
+	public Button btn;
+	public Button rest_Int;
+	public Text txt;
+	public int i;
+
+	// Use this for initialization
+	void Start ()
+	{
+		i = 0;
+
+		btn.onClick.AddListener(delegate()
+		{
+			i++;
+			txt.text = "功德數："+i;
+
+
+		});
+
+
+		rest_Int.onClick.AddListener(delegate ()
+		{
+			i=0;
+			txt.text = "功德數：" + i;
+
+		});
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
